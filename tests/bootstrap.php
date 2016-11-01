1a28c12c1531c588f079196b54c2392605adb306
Roelof Roos <github@roelof.io>
2016-11-01 03:20:26 +0100

    Added UTF-8 locales

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,18 @@
 <?php
 declare(strict_types=1);
 
+// Load Composer
 require_once __DIR__ . '/../src/vendor/autoload.php';
+
+// Set UTF-8 encoding
+setlocale(
+    LC_ALL,
+    'en_GB.UTF-8',
+    'en_GB@euro',
+    'en_GB@UTF-8',
+    'en_GB',
+    'en_US.UTF-8',
+    'en_US@euro',
+    'en_US@UTF-8',
+    'en_US'
+);
