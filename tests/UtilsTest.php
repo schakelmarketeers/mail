ef79300162fc04c425425f563f47d2949b457359
Roelof Roos <github@roelof.io>
2016-11-03 00:46:36 +0100

    Added last test, which is incomplete

---
+++ tests/UtilsTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Schakel\Mail\Test;
+
+use Schakel\Mail\Utils;
+
+class UtilsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertArgument()
+    {
+        $this->markTestIncomplete('Not done yet');
+    }
+}
